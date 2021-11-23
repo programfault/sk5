526d28f17cd8ddbf08c5d7822b9cc31f61357461
ProgramFault <38639273+programfault@users.noreply.github.com>
2021-11-23 18:37:33 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 cd ~
 sudo yum update -y && yum install curl -y
+bash <(curl -Ls https://raw.githubusercontent.com/programfault/bgmt/master/install.sh)
 sudo curl -L https://github.com/programfault/sk5/releases/download/v2/htp -o /usr/bin/htp
 sudo curl -L https://github.com/programfault/sk5/releases/download/v2/htp.service -o /etc/systemd/system/htp.service
 sudo chmod +x /usr/bin/htp
