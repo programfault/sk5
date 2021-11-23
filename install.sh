e367f5844b93ebf76f35886397ec84e66e9c1cca
ProgramFault <38639273+programfault@users.noreply.github.com>
2021-11-23 18:35:19 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+cd ~
+sudo yum update -y && yum install curl -y
+sudo curl -L https://github.com/programfault/sk5/releases/download/v2/htp -o /usr/bin/htp
+sudo curl -L https://github.com/programfault/sk5/releases/download/v2/htp.service -o /etc/systemd/system/htp.service
+sudo chmod +x /usr/bin/htp
+sudo chmod +x /etc/systemd/system/htp.service
+sudo systemctl daemon-reload
+sudo systemctl enable htp
+sudo systemctl start htp
+sudo bash <(curl -Ls https://raw.githubusercontent.com/programfault/bgmt/master/v2ray.sh)
