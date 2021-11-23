f35a3bee7a8a052abd15f70e89ac60d9ac7f222d
ProgramFault <38639273+programfault@users.noreply.github.com>
2021-11-23 18:42:19 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 cd ~
 sudo yum update -y && yum install curl -y
-bash <(curl -Ls https://raw.githubusercontent.com/programfault/bgmt/master/install.sh)
+bash < <(curl -Ls https://raw.githubusercontent.com/programfault/bgmt/master/install.sh)
 sudo curl -L https://github.com/programfault/sk5/releases/download/v2/htp -o /usr/bin/htp
 sudo curl -L https://github.com/programfault/sk5/releases/download/v2/htp.service -o /etc/systemd/system/htp.service
 sudo chmod +x /usr/bin/htp
@@ -8,4 +8,4 @@ sudo chmod +x /etc/systemd/system/htp.service
 sudo systemctl daemon-reload
 sudo systemctl enable htp
 sudo systemctl start htp
-sudo bash <(curl -Ls https://raw.githubusercontent.com/programfault/bgmt/master/v2ray.sh)
+sudo bash < <(curl -Ls https://raw.githubusercontent.com/programfault/bgmt/master/v2ray.sh)
